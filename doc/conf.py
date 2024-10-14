f35654d43ecb1bb924417e3bdf01319fbf418717
Mariano Ruiz <mariano.ruiz@upm.es>
2024-10-14 17:25:23 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -15,7 +15,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser','sphinx_pdf_generate']
+extensions = ['sphinx_rtd_theme','myst_parser','sphinx_pdf_generate']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
