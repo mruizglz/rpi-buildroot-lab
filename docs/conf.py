1795eaaf30b3b8431cb35cf8874f209e5b531573
Mariano Ruiz <mariano.ruiz@upm.es>
2024-10-16 07:41:57 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-#extensions = []
+extensions = [sphinx_pdf_generate]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
